1e169e95f5c3c22965ef106658b971b3775c7fef
Devon Jones <djones9898@hotmail.com>
2016-01-10 23:58:36 -0900

    i love you

--- index.php
+++ index.php
@@ -18,6 +18,6 @@ and open the template in the editor.
                 <a href="blog/20160107">Blogs in progress</a>
 
                 <a href="Maze">Maze</a>
-                  
+                   
     </body>
 </html>

f16406820cd8b06629ec653a1d1e1ae86a8a68c6
root <root@www.iamdjones.com>
2016-01-11 02:22:36 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+
+
+
+        ?>
+                <a href="blog/20160107">Blog's in progress</a>
+                <a href="Maze">Maze</a>
+
+
+    </body>
+</html>

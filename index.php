e3e43aa11417b5275b2203ee1dbef0f18c1b2799
Devon Jones <djones9898@hotmail.com>
2016-01-10 22:02:22 -0900

    copy root to new application Maze

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
-<?php
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
 
-error_reporting(-1);
-ini_set('display_errors', 1);
 
 
+        ?>
+                <a href="blog/20160107">Blog's in progress</a>
+                <a href="Maze">Maze</a>
 
 
-$layoutContents = file_get_contents("view/shared/_layout.html.php");
-
-print $layoutContents;
+    </body>
+</html>

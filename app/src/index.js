ea8cbc1a13be38bb3844e4c8c389a84e63f66703
Devon Jones <djones9898@hotmail.com>
2016-01-30 00:27:37 -0900

    remove shit files

--- app/src/index.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", "./app"], function (require, exports, App) {
-    var greeter = new App.Controller("Whatup");
-    greeter.greet();
-});
-//# sourceMappingURL=index.js.map
